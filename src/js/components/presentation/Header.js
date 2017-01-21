1cbca7fcbc7c85f8a013b1eecb4a655f1663b56e
William Paul Moore <wipmoore@outlook.com>
2017-01-21 14:09:32 +0000

    Rename signin to login

--- src/js/components/presentation/Header.js
+++ src/js/components/presentation/Header.js
@@ -1,9 +1,24 @@
 import React from "react";
 
+// Decided to import the needed functions at this level
+// rather that in the Layout.  Security is not really
+// paticipating in the Redux architecture, this is 
+// due to login component not being an easy fit.
+//
+// This should change but that would mean creating
+// a custom login component and accessing the services
+// in middleware which is going to take some time to
+// get right which is not a priority at this stage.
+//
+// ( That decisions will come back an bite me but hey 
+// ho you have to make some compromises )
+//
+// 
+
 import { hasIdToken } from '../../services/authentication-store';
 
+import Login from './Header/Login';
 import Logout from './Header/Logout';
-import Signin from './Header/Signin';
 import Title from "./Header/Title";
 
 
@@ -12,7 +27,7 @@ export default class Header extends React.Component {
   render() {
     let button = null;
     if ( !hasIdToken() ) {
-      button = <Signin />;
+      button = <Login />;
     } else {
       button = <Logout />;
     }
