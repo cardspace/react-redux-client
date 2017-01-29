3beb660f11a7cb2bcfda7b479a25e724ee504789
William Paul Moore <wipmoore@outlook.com>
2017-01-29 13:44:44 +0000

    Setup the staging environment

--- src/js/config/stage.js
+++ src/js/config/stage.js
@@ -8,9 +8,9 @@
 //  https://m.mattmclaugh.com/application-environment-configuration-with-webpack-238dd0015e42#.lba80s5so
 
 
-export const cards_url = 'https://cardspace-api-dev.herokuapp.com/v1/cards';
-export const card_url = 'https://cardspace-api-dev.herokuapp.com/v1/card';
+export const cards_url = 'https://cardspace-api-stage.herokuapp.com/v1/cards';
+export const card_url = 'https://cardspace-api-stage.herokuapp.com/v1/card';
 
-export const auth0_client_id = 'NP5BF3hkstyAEyg1J3boW6xXmOVGnHj3';
+export const auth0_client_id = 'Al6e64WfXSjkVWBuXUEInJldOYDB4pkx';
 export const auth0_domain = 'wipmoore.eu.auth0.com';
-export const auth0_redirectUrl = 'http://localhost:8080/user-authenticated';
+export const auth0_redirectUrl = 'https://cardspace-stage.firebaseapp.com/user-authenticated';
