b53414a537273b4bd2e9f19d5856996b5d24ce1d
William Paul Moore <wipmoore@outlook.com>
2017-01-30 11:46:21 +0000

    Correct dev environment errors

--- src/js/config/dev.js
+++ src/js/config/dev.js
@@ -3,12 +3,15 @@
 //  These are aliased in the appropriate webpack config the settings
 //  are accessed vi:
 //
-//  import config from 'config'
+//      import config from 'config'
+//
+//  NOTE - these setting are for running a local copy of the client against the
+//         the deployed development api.  
 //
 //  https://m.mattmclaugh.com/application-environment-configuration-with-webpack-238dd0015e42#.lba80s5so
 
-export const cards_url = 'http://localhost:8081/v1/cards';
-export const card_url = 'http://localhost:8081/v1/card';
+export const cards_url = 'https://cardspace-api-dev.herokuapp.com/v1/cards';
+export const card_url = 'https://cardspace-api-dev.herokuapp.com/v1/card';
 
 export const auth0_client_id = 'NP5BF3hkstyAEyg1J3boW6xXmOVGnHj3';
 export const auth0_domain = 'wipmoore.eu.auth0.com';
